16bef21202e846c9ae77db70153617d7cdc67244
Zhang Ran <info.zhangran@gmail.com>
2021-11-04 12:13:42 +0800

    Initial commit

---
+++ src/main/java/com/zenika/advancecicdpipingdemo/AdvanceCicdPipingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zenika.advancecicdpipingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceCicdPipingDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdvanceCicdPipingDemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zenika/advancecicdpipingdemo/AdvanceCicdPipingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.advancecicdpipingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceCicdPipingDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
